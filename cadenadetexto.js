beae8f16391c72a677b1cbf4d4a23eb01523df1b
alinson paez <alinsonpaez@gmail.com>
2023-04-26 16:19:52 +0100

    cadena de texto

---
+++ cadenadetexto.js
@@ -0,0 +1,21 @@
+const nombre = "Alinson";
+const apellido = "Paez";
+const estudiante = nombre + " " + apellido;
+const estudianteMayus = estudiante.toUpperCase();
+const estudianteMinus = estudiante.toLowerCase();
+const numLetras = estudiante.replace(/\s/g, '').length;
+const primeraLetraNombre = nombre.charAt(0);
+const ultimaLetraApellido = apellido.slice(-1);
+const estudianteSinEspacios = estudiante.replace(/\s/g, '');
+const nombreContenidoEnEstudiante = estudiante.includes(nombre);
+
+console.log(nombre);
+console.log(apellido);
+console.log(estudiante);
+console.log(estudianteMayus);
+console.log(estudianteMinus);
+console.log(numLetras);
+console.log(primeraLetraNombre);
+console.log(ultimaLetraApellido);
+console.log(estudianteSinEspacios);
+console.log(nombreContenidoEnEstudiante);
