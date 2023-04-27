b2dba928a129e35f73383704ddc143261b1de98a
alinson paez <alinsonpaez@gmail.com>
2023-04-27 21:30:02 +0100

    6

---
+++ 6.js
@@ -0,0 +1,9 @@
+// Crear un conjunto con los nombres de mi familia
+const familia = new Set(['Ali' , 'Maria', 'Nidian', 'Silio']);
+console.log(familia)
+// Agregar mi nombre duplicado al conjunto
+familia.add('Ali');
+console.log(familia)
+// Agregar el nombre "Javascript" al conjunto
+familia.add('Javascript');
+console.log(familia)
